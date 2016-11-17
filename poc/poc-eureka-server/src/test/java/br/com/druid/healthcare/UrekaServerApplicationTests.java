c80a7ea8b356ea945c71123215d7d79803c75828
Gustavo Madalon Castilha Bastos <gustavo.madalon@druid.com.br>
2016-11-17 21:09:32 -0200

    fds

--- poc/poc-eureka-server/src/test/java/br/com/druid/healthcare/UrekaServerApplicationTests.java
+++ poc/poc-eureka-server/src/test/java/br/com/druid/healthcare/UrekaServerApplicationTests.java
@@ -2,11 +2,11 @@ package br.com.druid.healthcare;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class UrekaServerApplicationTests {
 
 	@Test
