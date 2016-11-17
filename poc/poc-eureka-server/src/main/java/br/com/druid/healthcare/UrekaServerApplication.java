ca940ce20ca717f93fb2c2baea76ab6f5a842a4c
Gustavo Madalon Castilha Bastos <gustavo.madalon@druid.com.br>
2016-11-17 18:08:01 -0200

    Ciração do Eureka Server

---
+++ poc/poc-eureka-server/src/main/java/br/com/druid/healthcare/UrekaServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.druid.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UrekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrekaServerApplication.class, args);
+	}
+}
--- poc/poc-eureka-server/src/test/java/br/com/druid/healthcare/UrekaServerApplicationTests.java
+++ poc/poc-eureka-server/src/test/java/br/com/druid/healthcare/UrekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.druid;
+package br.com.druid.healthcare;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PocMicroserviceCustomerApplicationTests {
+public class UrekaServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
