ab53a2af99e8adef430d8b38e9e4bf6b81ed6a15
gmadalon <gustavo.madalon@druid.com.br>
2017-01-02 11:21:29 -0200

    New microservices projects

---
+++ api-gateway/src/main/java/br/com/druid/healthinsurance/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package br.com.druid.healthinsurance;
+
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+    
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }    
+}
