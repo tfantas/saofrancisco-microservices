340f271e542c748cfed517ff13800d6af3e3ff75
gmadalon <gustavo.madalon@druid.com.br>
2017-03-27 18:50:39 -0300

    Ajustes para o Oauth funcionar

--- api-gateway/src/main/java/br/com/druid/healthinsurance/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/com/druid/healthinsurance/ApiGatewayApplication.java
@@ -1,15 +1,19 @@
 package br.com.druid.healthinsurance;
 
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.Filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +21,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
@@ -24,64 +33,76 @@ import org.springframework.web.filter.ShallowEtagHeaderFilter;
 @EnableHystrix
 public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-    
-    @Bean
-    public Filter shallowEtagHeaderFilter() {
-        return new ShallowEtagHeaderFilter();
-    }
-    
-    @Bean
-    public SimpleFilter simpleFilter() {
-      return new SimpleFilter();
-    }
-    
-    @Bean
-    public ZuulFallbackProvider zuulFallbackProvider() {
-        return new ZuulFallbackProvider() {
-            @Override
-            public String getRoute() {
-                return "customer";
-            }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 
-            @Override
-            public ClientHttpResponse fallbackResponse() {
-                return new ClientHttpResponse() {
-                    @Override
-                    public HttpStatus getStatusCode() throws IOException {
-                        return HttpStatus.OK;
-                    }
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
 
-                    @Override
-                    public int getRawStatusCode() throws IOException {
-                        return 200;
-                    }
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
 
-                    @Override
-                    public String getStatusText() throws IOException {
-                        return "OK";
-                    }
+	@Bean
+	UserInfoRestTemplateCustomizer userInfoRestTemplateCustomizer(SpringClientFactory springClientFactory) {
+		return template -> {
+			AccessTokenProviderChain accessTokenProviderChain = Stream
+					.of(new AuthorizationCodeAccessTokenProvider(), new ImplicitAccessTokenProvider(),
+							new ResourceOwnerPasswordAccessTokenProvider(), new ClientCredentialsAccessTokenProvider())
+					.peek(tp -> tp.setRequestFactory(new RibbonClientHttpRequestFactory(springClientFactory)))
+					.collect(Collectors.collectingAndThen(Collectors.toList(), AccessTokenProviderChain::new));
+			template.setAccessTokenProvider(accessTokenProviderChain);
+		};
+	}
 
-                    @Override
-                    public void close() {
+	@Bean
+	public ZuulFallbackProvider zuulFallbackProvider() {
+		return new ZuulFallbackProvider() {
+			@Override
+			public String getRoute() {
+				return "customer";
+			}
 
-                    }
+			@Override
+			public ClientHttpResponse fallbackResponse() {
+				return new ClientHttpResponse() {
+					@Override
+					public HttpStatus getStatusCode() throws IOException {
+						return HttpStatus.OK;
+					}
 
-                    @Override
-                    public InputStream getBody() throws IOException {
-                        return new ByteArrayInputStream("fallback".getBytes());
-                    }
+					@Override
+					public int getRawStatusCode() throws IOException {
+						return 200;
+					}
 
-                    @Override
-                    public HttpHeaders getHeaders() {
-                        HttpHeaders headers = new HttpHeaders();
-                        headers.setContentType(MediaType.APPLICATION_JSON);
-                        return headers;
-                    }
-                };
-            }
-        };
-}
+					@Override
+					public String getStatusText() throws IOException {
+						return "OK";
+					}
+
+					@Override
+					public void close() {
+
+					}
+
+					@Override
+					public InputStream getBody() throws IOException {
+						return new ByteArrayInputStream("fallback".getBytes());
+					}
+
+					@Override
+					public HttpHeaders getHeaders() {
+						HttpHeaders headers = new HttpHeaders();
+						headers.setContentType(MediaType.APPLICATION_JSON);
+						return headers;
+					}
+				};
+			}
+		};
+	}
 }
---
+++ api-gateway/src/main/java/br/com/druid/healthinsurance/security/ResourceServerConfiguration.java
@@ -0,0 +1,68 @@
+package br.com.druid.healthinsurance.security;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) {
+		resources.tokenStore(new JwtTokenStore(jwtAccessTokenConverter));
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+	    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+	    Resource resource = new ClassPathResource("public.txt");
+	    String publicKey = null;
+	    try {
+	        publicKey = IOUtils.toString(resource.getInputStream());
+	    } catch (final IOException e) {
+	        throw new RuntimeException(e);
+	    }
+	    converter.setVerifierKey(publicKey);
+	    return converter;
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.csrf().disable().authorizeRequests().antMatchers("/services/oauth-server/**").permitAll().anyRequest().authenticated();
+
+		// @formatter:on
+	}
+
+}
