37ab3ecbfdc98592f648c3d5a5f48c2ec0d47425
Heitor Costa <heitor.costa@infobase.com.br>
2017-04-05 20:29:34 -0300

    # Restringindo acesso aos serviços somente com Authority

--- api-gateway/src/main/java/br/com/druid/healthinsurance/security/ResourceServerConfiguration.java
+++ api-gateway/src/main/java/br/com/druid/healthinsurance/security/ResourceServerConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -60,8 +61,57 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
-		http.csrf().disable().authorizeRequests().antMatchers("/services/oauth-server/**", "/eureka/**", "/services/access-control/user/regenerateConfirmation/**", "/services/access-control/user/registrationConfirm/**").permitAll().anyRequest().authenticated();
+		http
+			.csrf().disable().authorizeRequests()
+				.antMatchers("/services/oauth-server/**", "/eureka/**", "/services/access-control/user/regenerateConfirmation/**", "/services/access-control/user/registrationConfirm/**").permitAll().anyRequest().authenticated()
 
+                .antMatchers(HttpMethod.GET, "/services/access-control/**").hasAuthority("ACCESS_CONTROL_READ")
+                .antMatchers(HttpMethod.POST, "/services/access-control/**").hasAuthority("ACCESS_CONTROL_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/access-control/**").hasAuthority("ACCESS_CONTROL_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/access-control/**").hasAuthority("ACCESS_CONTROL_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/access-control/**").hasAuthority("ACCESS_CONTROL_WRITE")
+
+		        .antMatchers(HttpMethod.GET, "/services/company/**").hasAuthority("COMPANY_READ")
+                .antMatchers(HttpMethod.POST, "/services/company/**").hasAuthority("COMPANY_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/company/**").hasAuthority("COMPANY_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/company/**").hasAuthority("COMPANY_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/company/**").hasAuthority("COMPANY_WRITE")
+
+                .antMatchers(HttpMethod.GET, "/services/beneficiary/**").hasAuthority("BENEFICIARY_READ")
+                .antMatchers(HttpMethod.POST, "/services/beneficiary/**").hasAuthority("BENEFICIARY_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/beneficiary/**").hasAuthority("BENEFICIARY_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/beneficiary/**").hasAuthority("BENEFICIARY_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/beneficiary/**").hasAuthority("BENEFICIARY_WRITE")
+
+                .antMatchers(HttpMethod.GET, "/services/contract/**").hasAuthority("CONTRACT_READ")
+                .antMatchers(HttpMethod.POST, "/services/contract/**").hasAuthority("CONTRACT_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/contract/**").hasAuthority("CONTRACT_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/contract/**").hasAuthority("CONTRACT_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/contract/**").hasAuthority("CONTRACT_WRITE")
+
+                .antMatchers(HttpMethod.GET, "/services/address/**").hasAuthority("ADDRESS_READ")
+                .antMatchers(HttpMethod.POST, "/services/address/**").hasAuthority("ADDRESS_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/address/**").hasAuthority("ADDRESS_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/address/**").hasAuthority("ADDRESS_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/address/**").hasAuthority("ADDRESS_WRITE")
+
+                .antMatchers(HttpMethod.GET, "/services/document/**").hasAuthority("DOCUMENT_READ")
+                .antMatchers(HttpMethod.POST, "/services/document/**").hasAuthority("DOCUMENT_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/document/**").hasAuthority("DOCUMENT_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/document/**").hasAuthority("DOCUMENT_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/document/**").hasAuthority("DOCUMENT_WRITE")
+
+                .antMatchers(HttpMethod.GET, "/services/billing/**").hasAuthority("BILLING_READ")
+                .antMatchers(HttpMethod.POST, "/services/billing/**").hasAuthority("BILLING_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/billing/**").hasAuthority("BILLING_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/billing/**").hasAuthority("BILLING_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/billing/**").hasAuthority("BILLING_WRITE")
+
+                .antMatchers(HttpMethod.GET, "/services/price/**").hasAuthority("PRICE_READ")
+                .antMatchers(HttpMethod.POST, "/services/price/**").hasAuthority("PRICE_WRITE")
+                .antMatchers(HttpMethod.PUT, "/services/price/**").hasAuthority("PRICE_WRITE")
+                .antMatchers(HttpMethod.PATCH, "/services/price/**").hasAuthority("PRICE_WRITE")
+                .antMatchers(HttpMethod.DELETE, "/services/price/**").hasAuthority("PRICE_WRITE");
 		// @formatter:on
 	}
 }
